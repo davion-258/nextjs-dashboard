692e2870b04ed3d5fa9c356bb2531908f707d7a5
davion <hoangnam.itus@gmail.com>
2024-03-05 18:45:29 +0700

    fix authorize func

--- auth.ts
+++ auth.ts
@@ -20,18 +20,19 @@ export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [Credentials({
     async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
+        const parsedCredentials = z.object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
-          if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
-            if (passwordsMatch) return user;
-            console.log('Invalid credentials');
-            return null;
-          }
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+          console.log('Invalid credentials');
+          return null;
+        }
+        return null;
       },
   })]
 });
